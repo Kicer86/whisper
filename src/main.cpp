d061652b683da65f2d97afc29fa759a9ccc09244
Michał Walenciak <Kicer86@gmail.com>
2019-11-20 13:44:20 +0100

    comand line parsing basics + config dir option

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,26 @@
 
 #include <QApplication>
+#include <QCommandLineParser>
+#include <QStandardPaths>
 
 #include "main_window.hpp"
 
 int main(int argc, char** argv)
 {
     QApplication app(argc, argv);
+
+    const QString defaultConfigDir = QStandardPaths::writableLocation(QStandardPaths::AppDataLocation);
+    QCommandLineOption configDirOption({"c", "config"},
+                                       QString("Configuration directory to be used. Defaults to %1").arg(defaultConfigDir),
+                                       "directory",
+                                       defaultConfigDir);
+    QCommandLineParser cmdLineParser;
+    cmdLineParser.addOption(configDirOption);
+    cmdLineParser.addHelpOption();
+
+    cmdLineParser.process(app);
+
+    const QString configDir = cmdLineParser.value(configDirOption);
 
     MainWindow main_window;
     main_window.show();
