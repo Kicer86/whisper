f82439a74f04ff51c71f99309c6f36e5f3a19837
Michał Walenciak <Kicer86@gmail.com>
2019-12-03 20:03:22 +0100

    introducing IEncryptedConnection

---
+++ src/encryption/iencrypted_connection.hpp
@@ -0,0 +1,12 @@
+
+#ifndef IENCRYPTED_CONNECTION_HPP_INCLUDED
+#define IENCRYPTED_CONNECTION_HPP_INCLUDED
+
+
+struct IEncryptedConnection
+{
+    virtual ~IEncryptedConnection() = default;
+};
+
+
+#endif // IENCRYPTED_CONNECTION_HPP_INCLUDED
