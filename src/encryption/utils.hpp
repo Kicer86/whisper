8d8c22794309c85b75fcf86d31db68b4b7728bb7
Michał Walenciak <Kicer86@gmail.com>
2019-12-07 10:38:49 +0100

    introducing safe reinterpret cast

--- src/encryption/utils.hpp
+++ src/encryption/utils.hpp
@@ -2,6 +2,19 @@
 #ifndef UTILS_HPP_INCLUDED
 #define UTILS_HPP_INCLUDED
 
+namespace utils
+{
+    template<typename T, typename F>
+    constexpr T& binary_cast(F& from)
+    {
+        static_assert(sizeof(F) == sizeof(T), "type sizes need to be equal");
+
+        T* to = reinterpret_cast<T *>(&from);
+
+        return *to;
+    }
+
+}
 
 
 #endif // UTILS_HPP_INCLUDED
