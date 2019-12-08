f0b42be6ee21d860bc1d7db3cdfcc7ee3280dd43
Michał Walenciak <Kicer86@gmail.com>
2019-12-08 21:50:24 +0100

    introducing IKeysProvider

---
+++ src/encryption/ikeys_provider.hpp
@@ -0,0 +1,14 @@
+
+#ifndef IKEYS_PROVIDER_HPP_INCLUDED
+#define IKEYS_PROVIDER_HPP_INCLUDED
+
+
+struct IKeysProvider
+{
+    virtual ~IKeysProvider() = default;
+    virtual std::unique_ptr<Botan::Public_Key> ourPublicKey() const = 0;
+    virtual std::unique_ptr<Botan::Private_Key> ourPrivateKey() const = 0;
+};
+
+
+#endif // IKEYS_PROVIDER_HPP_INCLUDED
